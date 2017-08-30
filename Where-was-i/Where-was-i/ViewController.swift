e8c41b4784b57e04533feecf048f36c2f80fe676
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-29 23:52:40 -0400

    Organized project, ready to begin

---
+++ Where-was-i/Where-was-i/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Where-was-i
+//
+//  Created by Jose Melendez on 8/29/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
