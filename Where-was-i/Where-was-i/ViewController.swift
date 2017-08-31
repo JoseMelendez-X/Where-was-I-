6c610c73d3e50a35afe1b5666d25ee42126221a2
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-31 01:38:12 -0400

    User location is shown on a map

--- Where-was-i/Where-was-i/ViewController.swift
+++ Where-was-i/Where-was-i/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Location Manager 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        
+        //Info plist
+        locationManager.requestWhenInUseAuthorization()
+        
+        //Update location of the user
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        //If this is true then the else statement won't be executed
+        guard status == .authorizedWhenInUse else {
+            
+            print("Location not enabled!")
+            
+            return
+        }
+        
+        print("Location allowed")
+        
+        mapView.showsUserLocation = true
     }
 
 }
